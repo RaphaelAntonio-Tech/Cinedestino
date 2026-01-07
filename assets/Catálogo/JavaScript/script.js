346b0c8d6c16643d39ea6e016833f3912e7dcc4e
Raphael Tech <raphaelantonio.dev@gmail.com>
2026-01-07 11:01:58 -0300

    Update 1.6
    Ultimas atualizações antes do lançamento

--- assets/Catálogo/JavaScript/script.js
+++ assets/Catálogo/JavaScript/script.js
@@ -41,6 +41,8 @@ let image2 = document.querySelector('.image2')
 let image3 = document.querySelector('.image3')
 let image4 = document.querySelector('.image4')
 
+// links
+const Links = document.querySelectorAll('.btn-info-movie')
 //Inspuração
 
 function inspiracao() {
@@ -48,20 +50,32 @@ function inspiracao() {
     descMovie.innerHTML = `Filmes que tocam o coração e mostram como a fé, o amor e a esperança podem transformar vidas.<br> Histórias que inspiram o público a acreditar no bem e recomeçar com coragem.`
 
     // image 1
-    image.src = 'image/Além da Montanha.png'
-    titleDesc.innerHTML = 'Além da Montanha'
-   
+    image.src = 'image/Luz em Meu Caminho.png'
+    titleDesc.innerHTML = 'Luz em Meu Caminho'
+
+    //Primeiro Link
+    Links[0].href ='../filmes page/Filmes de Inspiração/Luz em Meu Caminho/index.html'
+    
     //image 2
     image2.src = 'image/Vento da Graça.png'
     titleDesc2.innerHTML = 'Vento da Graça'
+
+    //Segundo Link
+    Links[1].href = '../filmes page/Filmes de Inspiração/Vento da Graça/index.html'
 
     //image 3
     image3.src = 'image/Corações que Curam.png'
     titleDesc3.innerHTML = 'Corações que Curam'
 
+    //Terceiro Link
+    Links[2].href = '../filmes page/Filmes de Inspiração/Corações que Curam/index.html'
+
     //image 4 
     image4.src = 'image/A Luz do Perdão.png'
     titleDesc4.innerHTML = 'A Luz do Perdão'
+
+    //Quarto Link
+    Links[3].href = '../filmes page/Filmes de Inspiração/A Luz do Perdão/index.html'
 
 }
 
@@ -72,18 +86,30 @@ function Historico() {
     //image 1 
     image.src = 'image/A Cruz do Império.png'
     titleDesc.innerHTML = 'A Cruz do Império'
+    
+    //Primeiro Link
+   Links[0].href = '../filmes page/Filmes Histórico/A Cruz do Império/index.html'
 
     // Image 2
     image2.src ='image/O Manuscrito de Antioquia.png'
     titleDesc2.innerHTML = 'O Manuscrito de Antioquia'
 
+    //Segundo Link
+    Links[1].href = '../filmes page/Filmes Histórico/O Manuscrito de Antioquia/index.html'
+
     //Image 3 
     image3.src = 'image/Os Guardiões do Santuário.png'
     titleDesc3.innerHTML = 'Os Guardiões do Santuário'
 
+    //Terceiro Link
+    Links[2].href = '../filmes page/Filmes Histórico/Os Guardiões do Santuário/index.html'
+
     //image 4 
     image4.src = 'image/A Jornada de Helena.png'
     titleDesc4.innerHTML = 'A Jornada de Helena'
+
+    //Quarto Link
+    Links[3].href = '../filmes page/Filmes Histórico/A Jornada de Helena/index.html'
 }
 
 function Drama() {
@@ -94,17 +120,29 @@ function Drama() {
     image.src = 'image/O Peso do Silêncio.png'
     titleDesc.innerHTML = 'O Peso do Silêncio'
 
+    //Primeiro Link
+    Links[0].href = '../filmes page/FIlmes  Drama/O Peso do Silêncio/index.html'
+
     // image 2
     image2.src = 'image/Caminho de Lágrimas.png'
     titleDesc2.innerHTML = 'Caminho de Lágrimas'
+
+    //Segundo Link
+    Links[1].href = '../filmes page/FIlmes  Drama/Caminho de Lágrimas/index.html'
 
     // image 3
     image3.src = 'image/Promessa Quebrada.png'
     titleDesc3.innerHTML = 'Promessa Quebrada'
 
+    //Terceiro Link
+    Links[2].href = '../filmes page/FIlmes  Drama/Promessa Quebrada/index.html'
+
     // image 4
     image4.src = 'image/Entre a Dor e a Graça.png'
     titleDesc4.innerHTML = 'Entre a Dor e a Graça'
+
+    //Quarto Link
+    Links[3].href = '../filmes page/FIlmes  Drama/Entre a Dor e a Graça/index.html'
 
 }
 
@@ -116,17 +154,29 @@ function Superacao() {
     image.src = 'image/Chamas de Determinação.png'
     titleDesc.innerHTML = 'Chamas da Determinação'
 
+    //Primeiro Link
+    Links[0].href = '../filmes page/Filmes Superação/Chamas da Determinação/index.html'
+
     // image 2
     image2.src = 'image/Além do Medo.png'
     titleDesc2.innerHTML = 'Além do Medo'
+
+    //Segundo Link
+    Links[1].href = '../filmes page/Filmes Superação/Além do Medo/index.html'
 
     // image 3
     image3.src = 'image/Um Novo Amanhã.png'
     titleDesc3.innerHTML = 'Um Novo Amanhã'
 
+    //Terceiro Link
+    Links[2].href = '../filmes page/Filmes Superação/Um Novo Amanhã/index.html'
+
     // image 4
     image4.src = 'image/O Valor de Recomeçar.png'
     titleDesc4.innerHTML = 'O Valor de Recomeçar'
+
+    //Quarto Link
+    Links[3].href = '../filmes page/Filmes Superação/O Valor de Recomeçar/index.html'
 }
 
 function Religioso(){
@@ -137,36 +187,61 @@ function Religioso(){
     image.src = 'image/No Coração da Oração.png'
     titleDesc.innerHTML = 'No Coração da Oração'
 
+    //Primeiro Link
+    Links[0].href = '../filmes page/Filmes Religioso/No Coração da Oração/index.html'
+
     // image 2
     image2.src = 'image/Milagre no Deserto.png'
     titleDesc2.innerHTML = 'Milagre no Deserto'
+
+    //Segundo Link
+    Links[1].href = '../filmes page/Filmes Religioso/Milagre no Deserto/index.html'
 
     // image 3
     image3.src = 'image/Oração de Fátima.png'
     titleDesc3.innerHTML = 'Oração de Fátima'
 
+    //Terceiro Link
+    Links[2].href = '../filmes page/Filmes Religioso/Oração de Fátima/index.html'
+
     // image 4
     image4.src = 'image/O Canto dos Anjos.png'
     titleDesc4.innerHTML = 'O Canto dos Anjos'
+
+    //Quarto Link
+    Links[3].href = '../filmes page/Filmes Religioso/O Canto dos Anjos/index.html'
 }
 
 function Aventura() {
     movieTitle.innerHTML = 'Aventura '
     descMovie.innerHTML = 'Tramas cheias de movimento e descobertas, onde os protagonistas enfrentam desafios físicos e espirituais em jornadas que testam sua fé e determinação.'
 
+    
     //image 1
     image.src = 'image/Nas Trilhas da Fé.png'
     titleDesc.innerHTML = 'Nas Trilhas da Fé'
+    
+    //Primeiro Link
+    Links[0].href = '../filmes page/Filmes Aventura/Nas Trilhas da Fé/index.html'
 
     // image 2
     image2.src = 'image/A Ilha da Cruz Perdida.png'
     titleDesc2.innerHTML = 'A Ilha da Cruz Perdida'
 
+    //Segundo Link
+    Links[1].href = '../filmes page/Filmes Aventura/A Ilha da Cruz Perdida/index.html'
+
     // image 3
     image3.src = 'image/Rum ao Santuário.png'
     titleDesc3.innerHTML = 'Rumo ao Santuário'
 
+    //Terceiro Link
+    Links[2].href = '../filmes page/Filmes Aventura/Rumo ao Santuário/index.html'
+
     // image 4 
     image4.src = 'image/Guardião do Caminho.png'
     titleDesc4.innerHTML = 'Guardião do Caminho'
+
+    //Quarto Link
+    Links[3].href = '../filmes page/Filmes Aventura/Guardião do Caminho/index.html'
 }
---
+++ assets/filmes page/Filmes Histórico/A Jornada de Helena/index.html
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="shortcut icon" href="../../../Image/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Cinedestino - A Jornada de Helena</title>
+</head>
+
+<body>
+    <!-- Menu versão Desktop -->
+
+    <header class="header">
+
+        <!-- Lado esquerdo do menu -->
+        <div class="nav-left">
+            <h1 class="titulo-principal">Cinedestino</h1>
+
+            <nav class="menu-desk">
+                <a class="menu-item" href="../../../../index.html"><i class="fa-solid fa-house"></i>Página inicial</a>
+                <a class="menu-item" href="../../../Catálogo/index.html"><i class="fa-solid fa-gears"></i>Catálogo</a>
+            </nav>
+        </div>
+
+        <!-- Centro do menu (barra de pesquisa fica sempre centralizada) -->
+        <div class="nav-center">
+            <form class="pesquisa-form" role="search" aria-label="Pesquisar filmes">
+                <input type="search" name="Pesquisar" id="Pesquisar" placeholder="Pesquisar">
+                <button class="pesquisar-btn">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
+        </div>
+
+        <!-- Lado direito do menu -->
+        <div class="nav-right">
+            <a href="../../../Sobre/index.html" class="right-item"><i class="fa-solid fa-circle-info"></i>Sobre</a>
+            <a href="../A Jornada de Helena/index.html" class="botao-login">Entrar</a>
+        </div>
+    </header>
+
+
+    <!-- menu versão responsiva para tables e celulares -->
+    <header class="header-responsivo">
+        <h1 class="titulo-principal">Cinedestino</h1>
+        <nav class="menu">
+            <ul class="nav-list">
+                <li><a href="../../../../index.html" class="item-list"><i class="fa-solid fa-house"></i>Página inicial</a></li>
+                <li><a href="../../../Catálogo/index.html" class="item-list"><i class="fa-solid fa-gears"></i>Catálogo</a></li>
+                <li><a href="../../../Sobre/index.html" class="item-list"><i class="fa-solid fa-circle-info"></i>Sobre</a></li>
+                <li><a href="../../../Login e Cadastro/index.html" class="botao-login">Entrar</a></li>
+            </ul>
+        </nav>
+
+        <!-- Versão responsiva de menu para Tablets e celulares -->
+
+        <div class="pesquisa-responsivo">
+            <button class="botao-menu"><i class="fa-solid fa-bars"></i> </button>
+            <button class="botao-mobile-pesquisar"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+        <nav class="menu-lateral">
+            <ul>
+                <li><a href="../../../../index.html"><i class="fa-solid fa-house"></i> Página Inicial</a></li>
+                <li><a href="../../../Catálogo/index.html"><i class="fa-solid fa-gears"></i> Catálogo</a></li>
+                <li><a href="../../../Sobre/index.html"><i class="fa-solid fa-circle-info"></i> Sobre</a></li>
+                <li><a href="../../../Login e Cadastro/index.html"><i class="fa-solid fa-envelope"></i> Entrar</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Fundo escuro ao abrir o menu-->
+    <div class="background">
+
+    </div>
+
+    <!-- Poste do filme -->
+    <main class="conteudo">
+
+        <div class="container-poster">
+            <div class="poster">
+                <img src="../../../Image/A Jornada de Helena.jpg" alt="A Jornada de Helena" title="Imagem Criada pelo Gemni IA">
+            </div>
+            <div class="info-filme">
+                <h1 class="titulo-filme">A Jornada de Helena</h1>
+                <span>3h33 min | ⛰️ Viagem / Histórico</span>
+                <div class="nota-filme">
+                    <p><i class="fa-solid fa-star"></i>8,0/10</p>
+                </div>
+                <p class="sinopse">Uma cristã da antiguidade se arrisca para levar mensagens de esperança a vilas perseguidas.</p>
+                <div class="buttons">
+                    <a href="" class="button-trailer">Ver Trailer</a>
+                    <a href="" class="button-quiz">Ver Ahora</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="JavaScript/script.js"></script>
+</body>
+
+</html>
